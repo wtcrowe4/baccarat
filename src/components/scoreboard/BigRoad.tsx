b9e0c53eca7c16f6fe52f5d8ae05d44cd478fe4d
Thomas Crowe <wtcrowe4@outlook.com>
2024-09-06 15:04:08 -0400

    Add BigRoad component for displaying results in the scoreboard

---
+++ src/components/scoreboard/BigRoad.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './BigRoad.css'; // Make sure to create and style this CSS file
+
+interface BigRoadProps {
+    results: string[]; // Array of results, e.g., ['B', 'P', 'T', ...]
+}
+
+const BigRoad: React.FC<BigRoadProps> = ({ results }) => {
+    const renderCell = (result: string, index: number) => {
+        let className = '';
+        switch (result) {
+            case 'B':
+                className = 'banker';
+                break;
+            case 'P':
+                className = 'player';
+                break;
+            case 'T':
+                className = 'tie';
+                break;
+            default:
+                className = '';
+        }
+        return <div key={index} className={`cell ${className}`}>{result}</div>;
+    };
+
+    return (
+        <div className="big-road">
+            {results.map((result, index) => renderCell(result, index))}
+        </div>
+    );
+};
+
+export default BigRoad;
